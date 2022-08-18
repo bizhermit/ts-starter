a565652dc57a72240ffe161283e73a646dd0ee9f
bizhermit <rsenda@bizhermit.com>
2022-08-19 06:46:30 +0900

    fix

--- src/dist/create-next-app.ts
+++ src/dist/create-next-app.ts
@@ -9,8 +9,7 @@ type Mode = "all" | "frontend" | "backend" | "web" | "desktop";
 
 const createNextApp = async (wdir: string, mode: Mode = "all", separate = false, options?: ArgsOptions & {
   targetDir?: string;
-  apiBasePath?: string;
-  addDesktop?: boolean;
+  crossBasePath?: string;
   distFlat?: boolean;
 }) => {
   const { appName, platform } = analyzeArgsOptions(wdir, options);
@@ -30,36 +29,222 @@ const createNextApp = async (wdir: string, mode: Mode = "all", separate = false,
   const __rendererDistDir__ = "__rendererDistDir__";
   const rendererDistDir = ".renderer";
 
-  const desktopDeps = [
-    "fs-extra",
-    "electron-is-dev",
-    "electron-next",
-  ];
-  const desktopDevDeps = [
-    "@bizhermit/minifier",
-    "@types/fs-extra",
-    "electron",
-    "electron-builder",
-  ];
-  const backendDeps = [
-    "dotenv",
-    "express",
-    "express-session",
-    "helmet",
-  ];
-  const backendDevDeps = [
-    "@types/dotenv",
-    "@types/express",
-    "@types/express-session",
-  ];
-  const frontendDeps = [
-    "@bizhermit/react-addon",
-  ];
-
   const readReadmeFile = async (name: string) => {
     return (await readFile(path.join(getTemplateBaseDirname(), "next-app", name))).toString();
   }
-  const addDesktopPackageJsonProperties = (pkg: {[key: string]: any}) => {
+
+  if (mode === "web" ? separate : false) {
+    const frontendDir = "frontend";
+    const backendDir = "backend";
+    await createNextApp(wdir, frontendDir, false, {
+      appName: `${appName}-frontend`,
+      targetDir: frontendDir,
+      crossBasePath: `${appName}-backend`,
+      distFlat: true,
+    });
+    await createNextApp(wdir, backendDir, false, {
+      appName: `${appName}-backend`,
+      targetDir: backendDir,
+    });
+
+    const gitIgnoreLines = (await readFile(path.join(wdir, frontendDir, ".gitignore"))).toString().split(/\n/g);
+    gitIgnoreLines.splice(gitIgnoreLines.indexOf("# develop") - gitIgnoreLines.length + 1);
+    gitIgnoreLines.push(
+      ".vscode/settings.json",
+      `/${frontendDir}/${distDir}/`,
+      `${nextDistDir}`,
+      `${nexpressDistDir}`,
+    );
+    const pkg = await getPackageJson(wdir, { appName });
+    pkg.scripts = {
+      "frontend": `cd ${frontendDir} && npm run dev`,
+      "backend": `cd ${backendDir} && npm run nexpress`,
+      "dev": "npm run frontend | npm run backend",
+      "start:f": `cd ${frontendDir} && npm run next`,
+      "start:b": `cd ${backendDir} && npm run start`,
+      "start": "npm run start:b | npm run start:f"
+    };
+    await savePackageJson(wdir, pkg);
+    await writeFile(path.join(wdir, ".gitignore"), gitIgnoreLines.join("\n"));
+    removeGit(wdir);
+    return;
+  }
+
+  const targetDir = path.join(wdir, options?.targetDir ?? "");
+
+  spawnSync("npx", ["create-next-app", targetDir, "--ts", "--use-npm"], { shell: true, stdio: "inherit", cwd: wdir });
+  const configFilePath = path.join(targetDir, "next.config.js");
+  const configLines = (await readFile(configFilePath)).toString().split(/\n|\r\n/g);
+  const endStructIndex = configLines.findIndex(line => line.match(/^}(;|\s)*$/)) ?? 1;
+  if (!configLines[endStructIndex - 1].match(/,$/)) {
+    configLines[endStructIndex - 1] = configLines[endStructIndex - 1] + ",";
+  }
+  let configAddLines: Array<string> = [];
+  let envLines: Array<string> = [];
+
+  const gitignorePath = path.join(targetDir, ".gitignore");
+  let gitignoreContent = (await readFile(gitignorePath)).toString();
+  gitignoreContent = gitignoreContent.replace("/.next/", `/${nextDistDir}/`);
+  gitignoreContent += `\n# develop`;
+  const addGitignoreContents = (lines: Array<string>) => {
+    lines.forEach(line => {
+      gitignoreContent += `\n${line}`;
+    });
+  };
+  addGitignoreContents(["/.vscode/settings.json", `/${distDir}/`]);
+
+  const moveToSrc = async (fileName: string) => {
+    const srcFilePath = path.join(targetDir, fileName);
+    if (!existsSync(srcFilePath)) return;
+    await move(srcFilePath, path.join(targetDir, srcDir, fileName), { overwrite: true });
+  };
+  await moveToSrc("next-env.d.ts");
+  await moveToSrc("pages");
+  await moveToSrc("public");
+  rimraf.sync(path.join(targetDir, "styles"));
+
+  const pkg = await getPackageJson(targetDir, { appName });
+  pkg.name = appName;
+  pkg.version = "0.0.0-alpha.0";
+
+  const deps: Array<string> = ["@bizhermit/basic-utils"];
+  const devDeps: Array<string> = ["rimraf"];
+  let hasDesktop = false, hasBackend = false, hasFrontend = false;
+  const tsConfigExcludes = [
+    "node_modules",
+    "/.*",
+  ];
+  const poweredBy: Array<string> = [`[Next.js](https://nextjs.org/)`];
+  let appType = "", addonReadme = "";
+
+  // desktop
+  if (mode === "desktop" || mode === "all") {
+    hasDesktop = true;
+    tsConfigExcludes.push(distDir);
+    await generateTemplate(targetDir, "next-app/desktop");
+    await replaceTexts(path.join(targetDir, nextronDir, "main.ts"), [
+      { anchor: __appName__, text: appName },
+      { anchor: __srcDir__, text: srcDir },
+      { anchor: __mainDistDir__, text: mainDistDir },
+      { anchor: __rendererDistDir__, text: rendererDistDir },
+    ]);
+    await replaceTexts(path.join(targetDir, nextronDir, "tsconfig.json"), [
+      { anchor: __srcDir__, text: srcDir },
+      { anchor: __mainDistDir__, text: mainDistDir },
+    ]);
+    deps.push(
+      "fs-extra",
+      "electron-is-dev",
+      "electron-next",
+    );
+    devDeps.push(
+      "@bizhermit/minifier",
+      "@types/fs-extra",
+      "electron",
+      "electron-builder",
+    );
+  }
+
+  // backend
+  if (mode === "frontend" || mode === "desktop") {
+    await generateTemplate(targetDir, "next-app/backend");
+    rimraf.sync(path.join(targetDir, nexpressDir));
+    if (hasDesktop) await generateTemplate(targetDir, "next-app/backend-desktop");
+  } else {
+    hasBackend = true;
+    await generateTemplate(targetDir, "next-app/backend");
+    if (hasDesktop) await generateTemplate(targetDir, "next-app/backend-desktop");
+    await replaceTexts(path.join(targetDir, nexpressDir, "main.ts"), [
+      { anchor: __appName__, text: appName },
+      { anchor: __srcDir__, text: srcDir },
+    ]);
+    await replaceTexts(path.join(targetDir, nexpressDir, "tsconfig.json"), [
+      { anchor: __nexpressDistDir__, text: nexpressDistDir },
+    ]);
+    deps.push(
+      "dotenv",
+      "express",
+      "express-session",
+      "helmet",
+      "cors",
+      "csurf",
+      "cookie-parser",
+      "body-parser",
+    );
+    devDeps.push(
+      "@types/dotenv",
+      "@types/express",
+      "@types/express-session",
+      "@types/cors",
+      "@types/csurf",
+      "@types/cookie-parser",
+      "@types/body-parser"
+    );
+  }
+
+  // frontend
+  if (mode !== "backend") {
+    hasFrontend = true;
+    await generateTemplate(targetDir, "next-app/frontend");
+    if (hasDesktop) await generateTemplate(targetDir, "next-app/frontend-desktop");
+    deps.push(
+      "@bizhermit/react-addon",
+    );
+  }
+
+  // api
+  await generateTemplate(targetDir, "next-app/api");
+
+  if (mode === "backend") {
+    pkg.scripts = {
+      "build": "npx next build",
+    };
+  } else {
+    tsConfigExcludes.push(distDir);
+    const exportDir = options?.distFlat ? distDir : `${distDir}/${distNextDir}`;
+    pkg.scripts = {
+      "dev": "npx next dev -p 3000",
+      "build": "npx next build",
+      "next": "npm run build && npx next start -p 80",
+      "export": `npx rimraf ${exportDir} && npm run build && npx next export -o ${exportDir}`,
+    };
+  }
+
+  if (hasFrontend) {
+    addonReadme += await readReadmeFile("README.next.md");
+    tsConfigExcludes.push(nextDistDir);
+    pkg.scripts = {
+      ...pkg.scripts,
+    };
+  }
+  if (hasBackend) {
+    poweredBy.push(`[Express](https://expressjs.com/)`);
+    addonReadme += await readReadmeFile("README.nexpress.md");
+    tsConfigExcludes.push(nexpressDir);
+    tsConfigExcludes.push(nexpressDistDir);
+    addGitignoreContents([
+      `/${nexpressDistDir}/`,
+    ]);
+
+    pkg.scripts = {
+      ...pkg.scripts,
+      "nexpress": `npx rimraf ${nextDistDir} && npm run prestart && node ${nexpressDistDir}/main.js -d`,
+      "prestart": `npx rimraf ${nexpressDistDir} && npx tsc -p ${nexpressDir}/tsconfig.json`,
+      "start": `npm run build && node ${nexpressDistDir}/main.js`,
+    };
+  }
+
+  if (hasDesktop) {
+    poweredBy.push(`[Electron](https://www.electronjs.org/)`);
+    addonReadme += await readReadmeFile("README.nextron.md");
+    tsConfigExcludes.push(nextronDir);
+    tsConfigExcludes.push(mainDistDir);
+    tsConfigExcludes.push(rendererDistDir);
+    addGitignoreContents([
+      `/${mainDistDir}/`,
+      `/${rendererDistDir}/`,
+      `/resources/config.json`,
+    ]);
     const exportDir = options?.distFlat ? distDir : `${distDir}/${distPackDir}`;
     pkg.scripts = {
       ...pkg.scripts,
@@ -141,226 +326,6 @@ const createNextApp = async (wdir: string, mode: Mode = "all", separate = false,
       fs: false,
       path: false,
     };
-  };
-
-  if ((mode === "all" || mode === "web") ? separate : false) {
-    const addDesktop = mode === "all";
-    const frontendDir = "frontend";
-    const backendDir = "backend";
-    await createNextApp(wdir, frontendDir, false, {
-      appName: `${appName}-frontend`,
-      targetDir: frontendDir,
-      apiBasePath: addDesktop ? `${appName}-backend` : undefined,
-      addDesktop,
-      distFlat: true,
-    });
-    await createNextApp(wdir, backendDir, false, {
-      appName: `${appName}-backend`,
-      targetDir: backendDir,
-      addDesktop, 
-    });
-
-    const deps: Array<string> = [];
-    const devDeps: Array<string> = [];
-    const gitIgnoreLines = (await readFile(path.join(wdir, frontendDir, ".gitignore"))).toString().split(/\n/g);
-    gitIgnoreLines.splice(gitIgnoreLines.indexOf("# develop") - gitIgnoreLines.length + 1);
-    gitIgnoreLines.push(
-      ".vscode/settings.json",
-      `/${frontendDir}/${distDir}/`,
-      `${nextDistDir}`,
-      `${nexpressDistDir}`,
-    );
-    const pkg = await getPackageJson(wdir, { appName });
-
-    if (addDesktop) {
-      await generateTemplate(wdir, "next-app/desktop");
-      await replaceTexts(path.join(wdir, nextronDir, "main.ts"), [
-        { anchor: __appName__, text: appName },
-        { anchor: __srcDir__, text: `${frontendDir}/${srcDir}` },
-        { anchor: __mainDistDir__, text: `${mainDistDir}` },
-        { anchor: __rendererDistDir__, text: `${frontendDir}/${rendererDistDir}` },
-      ]);
-      await replaceTexts(path.join(wdir, nextronDir, "tsconfig.json"), [
-        { anchor: __srcDir__, text: `${frontendDir}/${srcDir}` },
-        { anchor: __mainDistDir__, text: `../${mainDistDir}` },
-      ]);
-      deps.push(...desktopDeps);
-      devDeps.push(
-        ...desktopDevDeps,
-        "rimraf"
-      );
-      gitIgnoreLines.push(
-        `${mainDistDir}`,
-        `${rendererDistDir}`,
-        `${frontendDir}/resources/config.json`,
-      );
-      addDesktopPackageJsonProperties(pkg);
-    } else {
-      pkg.scripts = {
-        "frontend": `cd ${frontendDir} && npm run dev`,
-        "backend": `cd ${backendDir} && npm run nexpress`,
-        "dev": "npm run frontend | npm run backend",
-        "start:f": `cd ${frontendDir} && npm run next`,
-        "start:b": `cd ${backendDir} && npm run start`,
-        "start": "npm run start:b | npm run start:f"
-      };
-    }
-    
-    await savePackageJson(wdir, pkg);
-    installLibs(wdir, deps, devDeps);
-    await writeFile(path.join(wdir, ".gitignore"), gitIgnoreLines.join("\n"));
-    removeGit(wdir);
-    return;
-  }
-
-  const targetDir = path.join(wdir, options?.targetDir ?? "");
-
-  spawnSync("npx", ["create-next-app", targetDir, "--ts", "--use-npm"], { shell: true, stdio: "inherit", cwd: wdir });
-  const configFilePath = path.join(targetDir, "next.config.js");
-  const configLines = (await readFile(configFilePath)).toString().split(/\n|\r\n/g);
-  const endStructIndex = configLines.findIndex(line => line.match(/^}(;|\s)*$/)) ?? 1;
-  if (!configLines[endStructIndex - 1].match(/,$/)) {
-    configLines[endStructIndex - 1] = configLines[endStructIndex - 1] + ",";
-  }
-  let configAddLines: Array<string> = [];
-  let envLines: Array<string> = [];
-
-  const gitignorePath = path.join(targetDir, ".gitignore");
-  let gitignoreContent = (await readFile(gitignorePath)).toString();
-  gitignoreContent = gitignoreContent.replace("/.next/", `/${nextDistDir}/`);
-  gitignoreContent += `\n# develop`;
-  const addGitignoreContents = (lines: Array<string>) => {
-    lines.forEach(line => {
-      gitignoreContent += `\n${line}`;
-    });
-  };
-  addGitignoreContents(["/.vscode/settings.json", `/${distDir}/`]);
-
-  const moveToSrc = async (fileName: string) => {
-    const srcFilePath = path.join(targetDir, fileName);
-    if (!existsSync(srcFilePath)) return;
-    await move(srcFilePath, path.join(targetDir, srcDir, fileName), { overwrite: true });
-  };
-  await moveToSrc("next-env.d.ts");
-  await moveToSrc("pages");
-  await moveToSrc("public");
-  rimraf.sync(path.join(targetDir, "styles"));
-
-  const pkg = await getPackageJson(targetDir, { appName });
-  pkg.name = appName;
-  pkg.version = "0.0.0-alpha.0";
-
-  const deps: Array<string> = ["@bizhermit/basic-utils"];
-  const devDeps: Array<string> = ["rimraf"];
-  let hasDesktop = false, hasBackend = false, hasFrontend = false;
-  const tsConfigExcludes = [
-    "node_modules",
-    "/.*",
-  ];
-  const poweredBy: Array<string> = [`[Next.js](https://nextjs.org/)`];
-  let appType = "", addonReadme = "";
-
-  // desktop
-  if (mode === "desktop" || mode === "all") {
-    hasDesktop = true;
-    tsConfigExcludes.push(distDir);
-    await generateTemplate(targetDir, "next-app/desktop");
-    await replaceTexts(path.join(targetDir, nextronDir, "main.ts"), [
-      { anchor: __appName__, text: appName },
-      { anchor: __srcDir__, text: srcDir },
-      { anchor: __mainDistDir__, text: mainDistDir },
-      { anchor: __rendererDistDir__, text: rendererDistDir },
-    ]);
-    await replaceTexts(path.join(targetDir, nextronDir, "tsconfig.json"), [
-      { anchor: __srcDir__, text: srcDir },
-      { anchor: __mainDistDir__, text: mainDistDir },
-    ]);
-    deps.push(...desktopDeps);
-    devDeps.push(...desktopDevDeps);
-  }
-
-  // backend
-  if (mode === "frontend" || mode === "desktop") {
-    await generateTemplate(targetDir, "next-app/backend");
-    rimraf.sync(path.join(targetDir, nexpressDir));
-    if (hasDesktop || options?.addDesktop) await generateTemplate(targetDir, "next-app/backend-desktop");
-  } else {
-    hasBackend = true;
-    await generateTemplate(targetDir, "next-app/backend");
-    if (hasDesktop || options?.addDesktop) await generateTemplate(targetDir, "next-app/backend-desktop");
-    await replaceTexts(path.join(targetDir, nexpressDir, "main.ts"), [
-      { anchor: __appName__, text: appName },
-      { anchor: __srcDir__, text: srcDir },
-    ]);
-    await replaceTexts(path.join(targetDir, nexpressDir, "tsconfig.json"), [
-      { anchor: __nexpressDistDir__, text: nexpressDistDir },
-    ]);
-    deps.push(...backendDeps);
-    devDeps.push(...backendDevDeps);
-  }
-
-  // frontend
-  if (mode !== "backend") {
-    hasFrontend = true;
-    await generateTemplate(targetDir, "next-app/frontend");
-    if (hasDesktop || options?.addDesktop) await generateTemplate(targetDir, "next-app/frontend-desktop");
-    deps.push(...frontendDeps);
-  }
-
-  // api
-  await generateTemplate(targetDir, "next-app/api");
-
-  if (mode === "backend") {
-    pkg.scripts = {
-      "build": "npx next build",
-    };
-  } else {
-    tsConfigExcludes.push(distDir);
-    const exportDir = options?.distFlat ? distDir : `${distDir}/${distNextDir}`;
-    pkg.scripts = {
-      "dev": "npx next dev -p 3000",
-      "build": "npx next build",
-      "next": "npm run build && npx next start -p 80",
-      "export": `npx rimraf ${exportDir} && npm run build && npx next export -o ${exportDir}`,
-    };
-  }
-
-  if (hasFrontend) {
-    addonReadme += await readReadmeFile("README.next.md");
-    tsConfigExcludes.push(nextDistDir);
-    pkg.scripts = {
-      ...pkg.scripts,
-    };
-  }
-  if (hasBackend) {
-    poweredBy.push(`[Express](https://expressjs.com/)`);
-    addonReadme += await readReadmeFile("README.nexpress.md");
-    tsConfigExcludes.push(nexpressDir);
-    tsConfigExcludes.push(nexpressDistDir);
-    addGitignoreContents([
-      `/${nexpressDistDir}/`,
-    ]);
-
-    pkg.scripts = {
-      ...pkg.scripts,
-      "nexpress": `npx rimraf ${nextDistDir} && npm run prestart && node ${nexpressDistDir}/main.js -d`,
-      "prestart": `npx rimraf ${nexpressDistDir} && npx tsc -p ${nexpressDir}/tsconfig.json`,
-      "start": `npm run build && node ${nexpressDistDir}/main.js`,
-    };
-  }
-
-  if (hasDesktop) {
-    poweredBy.push(`[Electron](https://www.electronjs.org/)`);
-    addonReadme += await readReadmeFile("README.nextron.md");
-    tsConfigExcludes.push(nextronDir);
-    tsConfigExcludes.push(mainDistDir);
-    tsConfigExcludes.push(rendererDistDir);
-    addGitignoreContents([
-      `/${mainDistDir}/`,
-      `/${rendererDistDir}/`,
-      `/resources/config.json`,
-    ]);
-    addDesktopPackageJsonProperties(pkg);
   }
 
   configAddLines = [
@@ -404,17 +369,15 @@ const createNextApp = async (wdir: string, mode: Mode = "all", separate = false,
         `    API_PROTOCOL: process.env.API_PROTOCOL,`,
         `    API_HOST_NAME: process.env.API_HOST_NAME,`,
         `    API_PORT: process.env.API_PORT,`,
-        `    API_BASE_PATH: process.env.BASE_PATH,`,
+        `    API_BASE_PATH: process.env.API_BASE_PATH,`,
         `  }`
       ];
-      const specifiedApi = StringUtils.isNotEmpty(options?.apiBasePath);
-      const envCommentStr = specifiedApi ? "" : "# ";
       envLines = [
         `# BASE_PATH=/${appName}`,
-        `${envCommentStr}API_PROTOCOL=${specifiedApi ? "http" : ""}`,
-        `${envCommentStr}API_HOST_NAME=${specifiedApi ? "localhost" : ""}`,
-        `${envCommentStr}API_PORT=80`,
-        `${envCommentStr}API_BASE_PATH=${specifiedApi ? `/${options?.apiBasePath}` : "$BASE_PATH"}`,
+        `# API_PROTOCOL=`,
+        `# API_HOST_NAME=`,
+        `# API_PORT=80`,
+        `# API_BASE_PATH=${options?.crossBasePath ? `/${options?.crossBasePath}` : "$BASE_PATH"}`,
       ];
       break;
     case "web":
